d292c0901d48a3607d70c12a72f7e3fd47804559
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-11-29 21:16:29 -0300

    [NEW] Organização do código para preparar entrega'

--- christofides.py
+++ christofides.py
@@ -15,20 +15,12 @@ def christofides(graph: NDArrayInt, src: np.int_ = 0):
             odd_degre_nodes.append(node)
     induced_subgraph = graph_nx.subgraph(odd_degre_nodes)
     min_weight_perfect_matching = nx.min_weight_matching(induced_subgraph, maxcardinality = True)
-
-    #multigrafo criado de duas formas, checar qual melhor
     eulerian_multigraph = nx.MultiGraph(mst)
-    
-    #eulerian_multigraph = nxMultiGraph()
-    #eulerian_multigraph.add_edges_from(mst.edges)
-    
     eulerian_multigraph.add_edges_from(min_weight_perfect_matching)
-
     eulerian_circuit = list(nx.eulerian_circuit(eulerian_multigraph))
     hamiltonian_cycle = []
     for u, _ in eulerian_circuit:
         if u in hamiltonian_cycle:
             continue
         hamiltonian_cycle.append(u)
-
     return hamiltonian_cycle + [src]
